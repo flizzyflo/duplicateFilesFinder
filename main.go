5b0e0178c93a4c9bd92dfa86dfa7b1d423a2da6c
flizzyflo <vip3r1@gmx.de>
2026-08-02 18:40:22 +0200

    added arguments, refactored arg checking. refactored finder function calls

--- main.go
+++ main.go
@@ -4,28 +4,48 @@ import (
 	"fmt"
 	"main/finder"
 	"os"
+	"slices"
 )
 
 // TODO: Add usage instruction
 func usage() {
-	fmt.Println()
+	fmt.Printf("\n[INFO]\tYou can provide several arguments to this function.\n")
+	fmt.Printf("[INFO]\tThe general use of this function is as follows:\n")
+	fmt.Printf("[INFO]\tExample usage:\tmain.go [args, optional] [filepath to start from]\n\n")
+	fmt.Printf("[ARGS]\t-help for this help overview; \t\t\t\tmain.go -help\n")
+	fmt.Printf("[ARGS]\t-log for enabling detailed logging; \t\t\tmain.go -log\n")
+	fmt.Printf("[ARGS]\t-perf for enabling performance measurement; \t\tmain.go -perf\n")
+	fmt.Printf("[ARGS]\t-pretty for enabling pretty output of results; \t\tmain.go -pretty\n")
+
 }
 
 // Todo: Add commantline args:
-// -l for long list.
-// -log for logs of steps,
 // -perf for performance measures
-// create git repo
 func main() {
-	f := finder.FileFinder{}
+
 	if len(os.Args) < 1 {
-		usage()
 		s := fmt.Errorf("[ERROR]: Not provided enough arguments")
 		fmt.Println(s)
+		usage()
 		return
 	}
 
-	f.Initialize("/Users/florianluebke/Desktop/stuff/")
+	var curArgs []string = os.Args
+	var log, perf, pretty bool
+
+	if slices.Contains(curArgs, "-help") {
+		usage()
+		return
+	}
+
+	log = slices.Contains(curArgs, "-log")
+	perf = slices.Contains(curArgs, "-perf")
+	pretty = slices.Contains(curArgs, "-pretty")
+
+	f := finder.FileFinder{}
+
+	f.Initialize("/Users/florianluebke/Desktop/stuff/", log, perf, pretty)
 	f.FindDuplicates()
-	// f.PrettifyResult()
+	f.PrintResults()
+
 }
