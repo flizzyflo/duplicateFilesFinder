a1fcae91bcd9c659cc05b38aefbd72c4d577501e
flizzyflo <vip3r1@gmx.de>
2026-08-08 09:23:46 +0200

    refactored filesperhash to own file. added docstrings. renamed api

--- finder/result.go
+++ finder/result.go
@@ -1,30 +1,22 @@
 package finder
 
-// TODO: Rename Strcuts and Vars
-type FilesPerHash struct {
-	filePaths                []string
-	fileContentHash          string
-	fileCount                int
-	fileContentHasDuplicates bool
-}
-
-func (hf *FilesPerHash) Append(hash string, filepath string) {
-	hf.filePaths = append(hf.filePaths, filepath)
-	hf.fileCount++
-	hf.fileContentHasDuplicates = len(hf.filePaths) > NO_DUPLICATES
-	hf.fileContentHash = hash
-
-}
-
+// Result represents the complete duplicate detection result.
+// It maps a file content hash to the corresponding collection of files
+// sharing that hash.
 type Result struct {
 	// key is hash
 	FilesPerHash map[string]*FilesPerHash
 }
 
-func (r *Result) init() {
+// new initializes the internal hash map used to store grouped files.
+// It must be called before adding any entries.
+func (r *Result) new() {
 	r.FilesPerHash = map[string]*FilesPerHash{}
 }
 
+// Add adds a file to the result under the specified content hash.
+// If the hash already exists, the file is appended to the existing group.
+// Otherwise, a new group is created for the hash.
 func (r *Result) Add(hash string, filepath string) {
 
 	files, ok := r.FilesPerHash[hash]
